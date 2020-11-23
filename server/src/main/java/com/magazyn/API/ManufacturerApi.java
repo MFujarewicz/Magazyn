484a3a4aac443ae6e09f5df89dccfa01c3a86bc4
artur00231 <artur.12.85.99.54@gmail.com>
2020-11-23 14:59:24 +0100

    Added more api endpoints

--- server/src/main/java/com/magazyn/API/ManufacturerApi.java
+++ server/src/main/java/com/magazyn/API/ManufacturerApi.java
@@ -1,7 +1,9 @@
 package com.magazyn.API;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,12 +20,22 @@ public class ManufacturerApi {
     }
 
     @GetMapping("/api/manufacturer/id/{id}")
-    public String getManufacturersByIdData(@PathVariable int id) {
+    public String getManufacturersDataById(@PathVariable int id) {
         return "Manufacturer id = " + id;
     }
 
     @GetMapping("/api/manufacturer/name/{name}")
-    public String getManufacturersByNameData(@PathVariable String name) {
-        return "Manufacturer anme = " + name;
+    public String getManufacturersDataByName(@PathVariable String name) {
+        return "Manufacturer name = " + name;
+    }
+
+    @DeleteMapping("/api/manufacturer/id/{id}")
+    public String delManufacturerById(@PathVariable int id) {
+        return "Delete manyfacturer id = " + id;
+    }
+
+    @PutMapping("/api/manufacturer/add/")
+    public String addManufacturer(int id) {
+        return "Delete manyfacturer id = " + id;
     }
 }
