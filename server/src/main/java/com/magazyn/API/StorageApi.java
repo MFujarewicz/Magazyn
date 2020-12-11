6d2e1c6efb8ff3b65d598ddcbb280179a4104080
Łukasz Szypski <250094@student.pwr.edu.pl>
2020-12-11 16:39:50 +0100

    fixed StorageApi

--- server/src/main/java/com/magazyn/API/StorageApi.java
+++ server/src/main/java/com/magazyn/API/StorageApi.java
@@ -1,6 +1,5 @@
 package com.magazyn.API;
 
-<<<<<<< HEAD
 import com.magazyn.API.exceptions.IllegalRequestException;
 import com.magazyn.API.exceptions.NoEndPointException;
 import com.magazyn.API.exceptions.NoResourceFoundException;
@@ -18,12 +17,23 @@ import javax.transaction.Transactional;
 import java.util.*;
 import java.util.Map.Entry;
 
+import com.magazyn.Storage.StorageManager;
+import com.magazyn.database.Product;
+import com.magazyn.database.ProductData;
+import com.magazyn.database.repositories.ProductDataRepository;
+import com.magazyn.database.repositories.ProductRepository;
+
+import javassist.bytecode.CodeAttribute.RuntimeCopyException;
+
 @RestController
 public class StorageApi {
     @Autowired
     ProductRepository productRepository;
     @Autowired
     ProductLocationRepository productLocationRepository;
+
+    @Autowired
+    StorageManager storage_manager;
 
     @RequestMapping(value = "/api/storage/**")
     public String showError() {
@@ -93,53 +103,10 @@ public class StorageApi {
             // sprawdzic czy miejsce jest zajeta jesli nie wywolac
             // WrongPlaceException
         } catch (NoSuchElementException ex) {
-=======
-import java.util.Optional;
-
-import com.magazyn.Storage.StorageManager;
-import com.magazyn.database.Product;
-import com.magazyn.database.ProductData;
-import com.magazyn.database.repositories.ProductDataRepository;
-import com.magazyn.database.repositories.ProductRepository;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import javassist.bytecode.CodeAttribute.RuntimeCopyException;
-
-@RestController
-public class StorageApi {
-    
-    @Autowired
-    ProductDataRepository product_data_repository;
-    @Autowired
-    ProductRepository product_repository;
-
-    @Autowired
-    StorageManager storage_manager;
-
-    @PutMapping("/api/storage/add")
-    void addProduct(@RequestParam int id) {
-        Optional<ProductData> requested_product_data = product_data_repository.findById(id);
-
-        if (requested_product_data.isEmpty()) {
-            throw new NoResourceFoundException();
-        }
-
-        try {
-        storage_manager.addNewProduct(requested_product_data.get());
-        } catch (RuntimeException exception) {
-            //TODO MAKE NEW EXCEPTION
-            System.err.println(exception.getMessage());
->>>>>>> remotes/origin/product_queue
             throw new NoResourceFoundException();
         }
     }
 
-<<<<<<< HEAD
     @GetMapping("/api/storage/place/{rack}/{place}")
     public String getProductById(@PathVariable int rack, @PathVariable int place) {
         try {
@@ -217,7 +184,24 @@ public class StorageApi {
 //        return  queryCreator.fromKeyWords(query_args);
         return "";
     }
-=======
+
+    @PutMapping("/api/storage/add")
+    void addProduct(@RequestParam int id) {
+        Optional<ProductData> requested_product_data = product_data_repository.findById(id);
+
+        if (requested_product_data.isEmpty()) {
+            throw new NoResourceFoundException();
+        }
+
+        try {
+            storage_manager.addNewProduct(requested_product_data.get());
+        } catch (RuntimeException exception) {
+            //TODO MAKE NEW EXCEPTION
+            System.err.println(exception.getMessage());
+            throw new NoResourceFoundException();
+        }
+    }
+
     @DeleteMapping("/api/storage/remove")
     void removeProduct(@RequestParam int id) {
         Optional<Product> requested_product = product_repository.findById(id);
@@ -237,5 +221,4 @@ public class StorageApi {
             throw new NoResourceFoundException();
         }
     }
->>>>>>> remotes/origin/product_queue
 }
